2c3c527733cef8fbf7c5a775ebcca11dabcb91e3
mluthfij <luthfujuliansyah48@gmail.com>
2023-02-14 11:50:27 +0700

    add username and phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,44 @@
 class User < ApplicationRecord
+  before_save { self.username = username.downcase }
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, 
+         :trackable ,authentication_keys: [:login]
+         
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 1, maximum: 100 }
+  validates :phone, presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 12, maximum: 20 }
+  validate :validate_username
+  validate :validate_phone
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email || self.phone
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value OR lower(phone) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email) || conditions.has_key?(:phone)
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def validate_phone
+    if User.where(email: phone).exists?
+      errors.add(:phone, :invalid)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_042221) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_044059) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,8 +24,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_042221) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["phone"], name: "index_users_on_phone", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
