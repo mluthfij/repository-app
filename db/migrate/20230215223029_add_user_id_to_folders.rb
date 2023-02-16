e5f532e065b4972b04440d155f3fa440f30aaccf
mluthfij <luthfujuliansyah48@gmail.com>
2023-02-16 07:36:02 +0700

    .

--- db/migrate/20230215223029_add_user_id_to_folders.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToFolders < ActiveRecord::Migration[7.0]
-  def change
-    add_column :folders, :user_id, :integer
-    add_index :folders, :user_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_223029) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_051108) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,14 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_223029) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "folders", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_folders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
