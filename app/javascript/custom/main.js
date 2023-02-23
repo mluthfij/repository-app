acdb2e5af04669faaf1f8d925dffb7fb0ff8c455
mluthfij <luthfujuliansyah48@gmail.com>
2023-02-23 20:35:32 +0700

    edit

--- app/javascript/custom/main.js
+++ app/javascript/custom/main.js
@@ -1,9 +1,15 @@
 const boxes = document.querySelectorAll('.box');
 const thewrapper = document.querySelector('.thewrapper');
 const menu = document.querySelector('.menu');
+// 
+const boxes2 = document.querySelectorAll('.box2');
+const thewrapper2 = document.querySelector('.thewrapper2');
+const menu2 = document.querySelector('.menu2');
+// 
 
 // show menu
 showmenu();
+showmenu2();
 function showmenu(){
     boxes.forEach(box =>{
         box.addEventListener('contextmenu',function(e){
@@ -24,5 +30,27 @@ thewrapper.addEventListener('click', hidmenu);
 function hidmenu(e){
     if(e.target.className != 'menu'){
         menu.style.display = 'none';
+    }
+}
+
+// box 2
+function showmenu2(){
+    boxes2.forEach(box2 =>{
+        box2.addEventListener('contextmenu',function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        const y = e.pageY;
+        const x = e.pageX
+        menu2.style.left = `${x}px`;
+        menu2.style.top = `${y}px`;
+        menu2.style.display = 'block';
+        })
+    })
+}
+// hidden menu
+thewrapper2.addEventListener('click', hidmenu2);
+function hidmenu2(e){
+    if(e.target.className != 'menu2'){
+        menu2.style.display = 'none';
     }
 }
